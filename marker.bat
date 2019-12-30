af18823bcee95fc434d6230a9406871a9e05ca13
Ashley Grenon <ashley.grenon@gmail.com>
2019-12-29 19:53:13 -0600

    Merge conflict leftovers

--- marker.bat
+++ marker.bat
@@ -1,9 +1,4 @@
 @echo off
 REM python C:\dev\image-marker\marker.py %*
-<<<<<<< HEAD
-python D:\dev\image-marker\marker.py -w 750 -f "Sniglet-ExtraBold.ttf" -c "(255, 255, 255, 190)" -t "Handmade\nwith\nAshley" %*
-pause 
-=======
 python D:\dev\image-marker\marker.py -w 750 -f "MyUnderwood.ttf" -c "(255, 255, 255, 190)" -t "Handmade with Ashley" %*
 pause 
->>>>>>> 3641bb2ace0dbbd5e58960a38ec9b289af5d6b18
